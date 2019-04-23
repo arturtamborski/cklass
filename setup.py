e6d3e7889a1b4f0bdbed7a3433af0f0c86a67c51
Artur Tamborski <tamborskiartur@gmail.com>
2019-04-23 04:11:12 +0200

    gotta go fast

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
             name = 'cklass',
-         version = '0.0.5',
+         version = '0.0.6',
          license = 'MIT',
           author = 'Artur Tamborski',
     author_email = 'tamborskiartur@gmail.com',
